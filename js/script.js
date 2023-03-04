61066f09e9038fac6422a8f1e949c774a80549f5
Reza Ghasemzadeh <rezaghasemzadeh1@gmail.com>
2023-03-04 12:04:34 +0330

    refactor remove items and add modal aparat

--- js/script.js
+++ js/script.js
@@ -1,110 +1,90 @@
-window.onload = (event) => {
-    chrome.storage.local.get('status', (data) => {
-        if (data.status === undefined || data.status == true) {
+chrome.storage.local.get('status', (data) => {
+    if (data.status === undefined || data.status == true) {
+        window.onload = (event) => {
             setIntervalX(function () {
                 removeItems();
             }, 500, 40);
         }
-    });
-}
-
-if (window.location.href.includes('google.com/search?q=')) {
-    let allTags = document.getElementsByTagName('video-voyager');
-    for (let index in allTags) {
-        if (allTags[index] instanceof HTMLElement) {
-            if (allTags[index].innerHTML.includes('aparat.com/v/')) {
-                let url = allTags[index].getElementsByTagName('div')[0].getElementsByTagName('a')[0].href;
-                let urlParts = new URL(url).pathname.split("/");
-                let id = urlParts[2];
-                let showTag = `<span class="aparat-preview" style="color :#1a0dab;font-size:14px;cursor:pointer;margin-top: 15px;display: block" data-id="${id}">نمایش سریع</span>`;
-                allTags[index].insertAdjacentHTML('beforeend', showTag);
-            }
-        }
-        //window.location.href = "https://www.aparat.com/video/video/embed/videohash/jp4B8/vt/frame";
+        addModalToAparat();
     }
-    $(".aparat-preview").on('click', function () {
-        console.log($(this).data('id'));
-    })
+});
+
+function addModalToAparat() {
+    if (window.location.href.includes('google.com/search?q=')) {
+        let allTags = document.getElementsByTagName('video-voyager');
+        for (let index in allTags) {
+            if (allTags[index] instanceof HTMLElement) {
+                if (allTags[index].innerHTML.includes('aparat.com/v/')) {
+                    let url = allTags[index].getElementsByTagName('div')[0].getElementsByTagName('a')[0].href;
+                    let urlParts = new URL(url).pathname.split("/");
+                    let id = urlParts[2];
+                    let showTag = `<span class="pab-aparat-preview" style="color :#1a0dab;font-size:14px;cursor:pointer;margin-top: 5px;margin-bottom:15px;display: block" data-id="${id}">نمایش سریع</span>`;
+                    allTags[index].insertAdjacentHTML('beforeend', showTag);
+                }
+            }
+            //window.location.href = "https://www.aparat.com/video/video/embed/videohash/jp4B8/vt/frame";
+        }
+        $(".pab-aparat-preview").on('click', function () {
+            let id = $(this).data('id');
+            // create the background modal div
+            const modal = document.createElement('div');
+            modal.classList.add('pab-modal');
+            // create the inner modal div with appended argument
+            const child = document.createElement('div');
+            child.classList.add('pab-modal-content');
+            child.innerHTML = `<style>.h_iframe-aparat_embed_frame{position:relative;}.h_iframe-aparat_embed_frame .ratio{display:block;width:100%;height:auto;}.h_iframe-aparat_embed_frame iframe{position:absolute;top:0;left:0;width:100%;height:100%;}</style><div class="h_iframe-aparat_embed_frame"><span style="display: block;padding-top: 57%"></span><iframe src="https://www.aparat.com/video/video/embed/videohash/${id}/vt/frame"  allowFullScreen="true" webkitallowfullscreen="true" mozallowfullscreen="true"></iframe></div>`;
+            // render the modal with child on DOM
+            modal.appendChild(child);
+            document.body.appendChild(modal);
+            modal.addEventListener('click', event => {
+                if (event.target.className === 'pab-modal') {
+                    const modal = document.querySelector('.pab-modal')
+                    if (modal) {
+                        modal.remove()
+                    }
+                }
+            })
+        })
+
+    }
 }
 
 function removeItems() {
-    // yektanet
-    const yektanetClass = document.querySelectorAll("[class*=yn-]");
-    const yektanetId = document.querySelectorAll("[id*=yn-]");
-    //sanjagh
-    const sanjaghClass = document.querySelectorAll("[class*=sanjagh]");
-    const sanjaghId = document.querySelectorAll("[id*=sanjagh]");
-    //sabavision
-    const sabavisionClass = document.querySelectorAll("[class*=sabavision]");
-    const sabavisionId = document.querySelectorAll("[id*=sabavision]");
-    //najva
-    const najvaClass = document.querySelectorAll("[class*=najva]");
-    const najvaId = document.querySelectorAll("[id*=najva]");
-    //congoro
-    const congoroClass = document.querySelectorAll("[class*=congoro]");
-    const congoroId = document.querySelectorAll("[id*=congoro]");
-    //mediaad
-    const mediaadClass = document.querySelectorAll("[class*=mediaad]");
-    const mediaadId = document.querySelectorAll("[id*=mediaad]");
+    const items = [
+        // yektanet
+        "[class*=yn-]",
+        "[id*=yn-]",
+        //sanjagh
+        "[class*=sanjagh]",
+        "[id*=sanjagh]",
+        //sabavision
+        "[class*=sabavision]",
+        "[id*=sabavision]",
+        //najva
+        "[class*=najva]",
+        "[id*=najva]",
+        //congoro
+        "[class*=congoro]",
+        "[id*=congoro]",
+        //mediaad
+        "[class*=mediaad]",
+        "[id*=mediaad]",
+        //kaprila
+        "[class*=kaprila]",
+        "[id*=kaprila]",
+        //Adv
+        "[class*=adv]",
+        "[id*=adv]",
+        //Ads
+        "[class*=ads]",
+        "[id*=ads]"
+    ];
 
-    //kaprila
-    const kaprilaClass = document.querySelectorAll("[class*=kaprila]");
-    const kaprilaId = document.querySelectorAll("[id*=kaprila]");
-
-    [].forEach.call(yektanetClass, function (el) {
-        el.innerHTML = "";
-    });
-
-    [].forEach.call(yektanetId, function (el) {
-        el.innerHTML = "";
-    });
-
-    [].forEach.call(sanjaghClass, function (el) {
-        el.innerHTML = "";
-    });
-
-    [].forEach.call(sanjaghId, function (el) {
-        el.innerHTML = "";
-    });
-
-    [].forEach.call(sabavisionClass, function (el) {
-        el.innerHTML = "";
-    });
-
-    [].forEach.call(sabavisionId, function (el) {
-        el.innerHTML = "";
-    });
-
-    [].forEach.call(najvaClass, function (el) {
-        el.innerHTML = "";
-    });
-
-    [].forEach.call(najvaId, function (el) {
-        el.innerHTML = "";
-    });
-
-    [].forEach.call(congoroClass, function (el) {
-        el.innerHTML = "";
-    });
-
-    [].forEach.call(congoroId, function (el) {
-        el.innerHTML = "";
-    });
-
-    [].forEach.call(mediaadClass, function (el) {
-        el.innerHTML = "";
-    });
-
-    [].forEach.call(mediaadId, function (el) {
-        el.innerHTML = "";
-    });
-
-    [].forEach.call(kaprilaClass, function (el) {
-        el.innerHTML = "";
-    });
-
-    [].forEach.call(kaprilaId, function (el) {
-        el.innerHTML = "";
+    items.forEach(function (item) {
+        let element = document.querySelectorAll(item);
+        [].forEach.call(element, function (el) {
+            el.innerHTML = "";
+        });
     });
 }
 
